31848cf0ee9af9c80e936afeed0b4fb23d251f46
leoulendryas <leoulendryas@gmail.com>
2026-01-29 10:32:43 +0300

    api: update wishlist fetch logic

--- ecom-frontend/ecom-site/src/pages/api/wishlist.ts
+++ ecom-frontend/ecom-site/src/pages/api/wishlist.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/wishlist`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/wishlist`, {
             method: 'GET',
             headers: {
                 'Authorization': req.headers.authorization || ''
